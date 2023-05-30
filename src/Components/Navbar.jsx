07b0c19ce367650994c7df33fc393bf115c2b514
manaboy-07 <manassehoruebor@gmail.com>
2023-05-30 23:04:36 +0100

    fixed bug

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 
 function Navbar() {
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(true);
   const handleNav = () => {
     setShowNav((prevState) => !prevState);
   };
@@ -31,44 +31,45 @@ function Navbar() {
           </div>
         </section>
       </div>
-
-      <div
-        className={`mobile-nav z-50 bg-black block md:hidden m-0 p-0 ${
-          showNav ? "actives" : "inactive"
-        }`}>
-        <div className='flex flex-col m-4 p-5'>
-          <div className='flex justify-between items-center'>
-            {" "}
-            <section className='text-2xl text-primary'>Mana-Dev</section>
-          </div>
-          <section>
-            <div className=''>
-              <ul className='flex flex-col mt-2'>
-                <li
-                  className='cursor-pointer text-xl mx-3 mt-5'
-                  onClick={() => setShowNav(false)}>
-                  <a href='#home'>Home</a>
-                </li>
-                <li
-                  className='cursor-pointer text-xl  mx-3 mt-5'
-                  onClick={() => setShowNav(false)}>
-                  <a href='#about'>About</a>
-                </li>
-                <li
-                  className='cursor-pointer text-xl  mx-3 mt-5'
-                  onClick={() => setShowNav(false)}>
-                  <a href='#projects'>Projects</a>
-                </li>
-                <li
-                  className='cursor-pointer text-xl  mx-3 mt-5'
-                  onClick={() => setShowNav(false)}>
-                  <a href='#contact'>Contact</a>
-                </li>
-              </ul>
+      <div className={`mobile ${showNav ? "actives" : "inactive"}`}>
+        <div
+          className={`mobile-nav z-50 bg-black block md:hidden m-0 p-0 ${
+            showNav ? "actives" : "inactive"
+          }`}>
+          <div className='flex flex-col m-4 p-5'>
+            <div className='flex justify-between items-center'>
+              {" "}
+              <section className='text-2xl text-primary'>Mana-Dev</section>
             </div>
-          </section>
+            <section>
+              <div className=''>
+                <ul className='flex flex-col mt-2'>
+                  <li
+                    className='cursor-pointer text-xl mx-3 mt-5'
+                    onClick={() => setShowNav(false)}>
+                    <a href='#home'>Home</a>
+                  </li>
+                  <li
+                    className='cursor-pointer text-xl  mx-3 mt-5'
+                    onClick={() => setShowNav(false)}>
+                    <a href='#about'>About</a>
+                  </li>
+                  <li
+                    className='cursor-pointer text-xl  mx-3 mt-5'
+                    onClick={() => setShowNav(false)}>
+                    <a href='#projects'>Projects</a>
+                  </li>
+                  <li
+                    className='cursor-pointer text-xl  mx-3 mt-5'
+                    onClick={() => setShowNav(false)}>
+                    <a href='#contact'>Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </div>
         </div>
-        <div className='flex items-center justify-center'>
+        <div className='flex bg-black items-center justify-center'>
           <div
             className='text-2xl text-primary cursor-pointer '
             onClick={handleNav}>
